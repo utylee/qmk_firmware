6399faa9953d3a684b26ad592e921d34fbc0d50e
utylee <utylee@gmail.com>
2020-08-18 00:22:01 +0900

    rgb test

--- keyboards/bm40hsrgb/keymaps/utylee/keymap.c
+++ keyboards/bm40hsrgb/keymaps/utylee/keymap.c
@@ -276,10 +276,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-  state = update_tri_layer_state(state, _LOWER_WOW, _RAISE_WOW, _ADJUST_WOW);
-  return state;
-  //return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+	state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+	state = update_tri_layer_state(state, _LOWER_WOW, _RAISE_WOW, _ADJUST_WOW);
+	switch (get_highest_layer(state)) {
+		case _RAISE:
+			rgblight_setrgb (0x00,  0x00, 0xFF);
+			break;
+		case _LOWER:
+			rgblight_setrgb (0xFF,  0x00, 0x00);
+			break;
+		case _ADJUST:
+			rgblight_setrgb (0x7A,  0x00, 0xFF);
+			break;
+		default: //  for any other layers, or the default layer
+			rgblight_setrgb (0x00,  0xFF, 0xFF);
+			break;
+		}
+	return state;
+	//return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
