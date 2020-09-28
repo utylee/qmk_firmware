b0807a8f2c573873837fe80aaccaa8fb39adde28
utylee <utylee@gmail.com>
2020-09-29 00:34:16 +0900

    added spacefn2 mac, and delete duplicated spacefn layer

--- keyboards/bm40hsrgb/keymaps/utylee/keymap.c
+++ keyboards/bm40hsrgb/keymaps/utylee/keymap.c
@@ -174,23 +174,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PSCR,  KC_1, KC_2,    KC_3,   XXXXXXX,  WOW,  MAGIC_TOGGLE_NKRO ,XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    _______, 
       XXXXXXX,  KC_4, KC_5,  KC_6,  QWERTY,  QWERTY,  KC_PGDN,  KC_LEFT, KC_DOWN,  KC_UP,       KC_RIGHT,   _______, 
       XXXXXXX,  KC_7, KC_8,  KC_9,   KC_0, _______ , _______, _______, XXXXXXX,XXXXXXX  ,     XXXXXXX,    XXXXXXX, 
-      XXXXXXX,  KC_MPLY, KC_MPRV, KC_MNXT,   _______,  _______,  _______, KC_MUTE, KC_VOLD, KC_VOLU,  XXXXXXX
-),
-  /* Space fn
-  * ,-----------------------------------------------------------------------------------.
-  * |PRNT SC|      |  UP  |      |      |      |      |      |      |      |      |SLEEP |
-  * |-------+------+------+------+------+-------------+------+------+------+------+------|
-  * |       | LEFT | DOWN | RIGHT|      |      |      | LEFT | DOWN |  UP  | RIGHT|      |
-  * |-------+------+------+------+------+------|------+------+------+------+------+------| 
-  * |       |      |      |      |      |      |      |      |      |      |      |      |
-  * |-------+------+------+------+------+------+------+------+------+------+------+------|
-  * |       |      |      |      |      |             |      | MUTE |VOLDWN|VOL UP|      |
-  * `-----------------------------------------------------------------------------------'
-  */
-  [_SPACE_FN] = LAYOUT_planck_mit(
-      KC_PSCR,  KC_1, KC_2,    KC_3,   XXXXXXX,  WOW,  MAGIC_TOGGLE_NKRO ,XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    _______, 
-      XXXXXXX,  KC_4, KC_5,  KC_6,  QWERTY,  QWERTY,  KC_PGDN,  KC_LEFT, KC_DOWN,  KC_UP,       KC_RIGHT,   _______, 
-      XXXXXXX,  KC_7, KC_8,  KC_9,   KC_0, _______ , _______, _______, XXXXXXX,XXXXXXX  ,     XXXXXXX,    XXXXXXX, 
       XXXXXXX,  KC_MPLY, KC_MPRV, KC_MNXT,   XXXXXXX,  XXXXXXX,  XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU,  XXXXXXX
 ),
 
@@ -207,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_SPACE_FN2] = LAYOUT_planck_mit(
       KC_PSCR,  KC_F1, KC_F2,    KC_F3,   KC_F4,  WOW,  MAGIC_TOGGLE_NKRO , KC_HOME,KC_PGDN,  KC_PGUP, KC_END    ,    _______, 
-      XXXXXXX,  KC_F5, KC_F6,  KC_F7,  KC_F8,  QWERTY,  QWERTY,  KC_LEFT, KC_DOWN,  KC_UP,       KC_RIGHT,   _______, 
+      XXXXXXX,  KC_F5, KC_F6,  KC_F7,  KC_F8,  QWERTY,  QWERTY,  KC_LEFT, KC_DOWN,  KC_UP,       KC_RIGHT,   MAC, 
       XXXXXXX,  KC_F9, KC_F10,  KC_F11,   KC_F12, ARROW , ARROW, _______, XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX, 
       XXXXXXX,  KC_MPLY, KC_MPRV, KC_MNXT,   XXXXXXX,  XXXXXXX,  XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU,  XXXXXXX
 ),
