4c683957152631ab4c9e345da15a3a35682dc8ed
utylee <utylee@gmail.com>
2020-10-12 23:19:53 +0900

    quot to tap right shift added

--- keyboards/bm40hsrgb/keymaps/utylee/keymap.c
+++ keyboards/bm40hsrgb/keymaps/utylee/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_mit(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, LT(_FUNC, KC_COMM), KC_DOT,  KC_SLSH, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, LT(_FUNC, KC_COMM), KC_DOT,  KC_SLSH, SFT_T(KC_QUOT),
     _______, KC_LALT, KC_LGUI, LOWER,   SPACE_FN, SPACE_FN2, SPACE_FN, RAISE,   KC_RALT, KC_RCTRL,   _______
 ),
 
@@ -340,6 +340,12 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 			return 50;
 			break;	
 		*/
+		case CTL_T(KC_ESC):
+			return 50;
+			break;
+		case SFT_T(KC_QUOT):
+			return 50;
+			break;
 		case SPACE_FN2:
 			return 50;
 			break;
