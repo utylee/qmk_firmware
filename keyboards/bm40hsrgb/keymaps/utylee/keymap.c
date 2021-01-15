6c3bcb020fe3c491a1bede19a95d3e6bf61f0a8d
utylee <utylee@gmail.com>
2021-01-15 22:56:40 +0900

    keymap added function(,) media hotkey

--- keyboards/bm40hsrgb/keymaps/utylee/keymap.c
+++ keyboards/bm40hsrgb/keymaps/utylee/keymap.c
@@ -211,7 +211,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_ASTR,  KC_7, KC_8,  KC_9, KC_0, KC_MINUS, KC_PLUS,
     KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_EQL,   KC_4, KC_5,  KC_6, KC_EQL, KC_LBRC, KC_RBRC,
     _______, KC_F9,   KC_F10,   KC_F11,   KC_F12,  _______, KC_1, KC_2,  KC_3, XXXXXXX, KC_BSLS, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______,  KC_MPLY, KC_MPRV, KC_MNXT, _______, _______
 ),
 
 /* Lower
