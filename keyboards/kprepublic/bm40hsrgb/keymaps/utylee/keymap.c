46cb0eaa04ad1059879691bda3ee230f34f208e0
utylee <utylee@gmail.com>
2022-09-03 22:30:26 +0900

    chaged: right shift, ctrl, alt mod-tap added for left hand trackball use

--- keyboards/kprepublic/bm40hsrgb/keymaps/utylee/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/utylee/keymap.c
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CTL_T(KC_ESC),  LALT_T(KC_A),    LSFT_T(KC_S),    LGUI_T(KC_D),    LCTL_T(KC_F),    KC_G,    KC_H,   LCTL_T(KC_J),  RGUI_T(KC_K),   RSFT_T(KC_L),   RALT_T(KC_SCLN), KC_ENT,
     /* KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_NUM, KC_V),    KC_B,    KC_N,    LT(_FUNC, KC_M), KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, */
     /* KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_NUM, KC_V),    KC_B,    KC_N,    LT(_FUNC, KC_M), KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, */
-	KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_NUM, KC_V),    KC_B,    KC_N,    LT(_FUNC, KC_M), LT(_FUNC, KC_COMM), KC_DOT,  KC_SLSH, KC_QUOT,
-    _______, KC_LALT, KC_LGUI, LOWER,   CTL_T(KC_SPC), SPACE_FN2, SPACE_FN, RAISE,   KC_RALT, KC_RCTRL,   _______
+	KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_NUM, KC_V),    KC_B,    KC_N,    LT(_FUNC, KC_M), LT(_FUNC, KC_COMM), KC_DOT,  LCTL_T(KC_SLSH), RSFT_T(KC_QUOT),
+    _______, KC_LALT, KC_LGUI, LOWER,   CTL_T(KC_SPC), SPACE_FN2, SPACE_FN, RAISE,   KC_RALT, LALT_T(KC_RCTRL),   _______
 ),
 
 /* [_QWERTY] = LAYOUT_planck_mit( */
@@ -160,9 +160,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_WOW] = LAYOUT_planck_mit(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, LT(_FUNC, KC_COMM), KC_DOT,  KC_SLSH, SFT_T(KC_QUOT),
-    _______, KC_LALT, RAISE_WOW, LOWER_WOW, KC_SPC, SPACE_FN2_R , ESC_FN2 ,  RAISE,   KC_RALT, KC_LGUI, _______   
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, LT(_FUNC, KC_COMM), LGUI_T(KC_DOT),  LCTL_T(KC_SLSH), SFT_T(KC_QUOT),
+    _______, KC_LALT, RAISE_WOW, LOWER_WOW, KC_SPC, SPACE_FN2_R , ESC_FN2 ,  RAISE,   KC_RALT, LALT_T(KC_RCTL), _______   
+    /* _______, KC_LALT, RAISE_WOW, LOWER_WOW, KC_SPC, SPACE_FN2_R , ESC_FN2 ,  RAISE,   KC_RALT, KC_LGUI, _______ */   
     /* CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, */
+
 ),
     /* _______, KC_LALT, KC_LALT, LOWER_WOW, KC_SPC, SPACE_FN2, KC_SPC ,  RAISE,   KC_RALT, KC_LGUI, _______ */   
     /* _______, KC_LALT, KC_LALT, LOWER_WOW, KC_SPC, SPACE_FN2, NUM ,  RAISE,   KC_RALT, KC_LGUI, _______ */   
