8c0e0cdf125fa6165e5849320aae49805dcda354
utylee <utylee@gmail.com>
2024-08-14 16:12:30 +0900

    chg: wsl chg something?

--- keyboards/kprepublic/bm40hsrgb/keymaps/utylee/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/utylee/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_LSFT, KC_Z, KC_X, KC_C, LT(_NUM, KC_V), KC_B, KC_N, LT(_FUNC, KC_M), LT(_FUNC, KC_COMM), KC_DOT, RCTL_T(KC_SLSH), RSFT_T(KC_QUOT),
                                   /* _______, KC_LALT, KC_LGUI, LOWER,   CTL_T(KC_SPC), SPACE_FN2, SPACE_FN, RAISE,   KC_RALT, LALT_T(KC_RCTRL),   _______ */
                                   /* _______, KC_LALT, KC_LCTRL, LOWER,   CTL_T(KC_SPC), SPACE_FN2, SPACE_FN, RAISE,   KC_LCTRL, KC_LALT,   _______ */
-                                  _______, KC_LALT, RAISE_WOW, LOWER, CTL_T(KC_SPC), SPACE_FN2, SPACE_FN, RAISE, KC_LCTRL, KC_LALT, _______),
+                                  _______, KC_LALT, RAISE_WOW, LOWER, CTL_T(KC_SPC), SPACE_FN2,/*center*/ SPACE_FN, RAISE, KC_LCTRL, KC_LALT, _______),
 
     /* [_QWERTY] = LAYOUT_planck_mit( */
     /*     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, */
@@ -140,7 +140,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_MAC] = LAYOUT_planck_mit(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                /*{KC_LCTL,  KC_A,    KC_S,    KC_D,    LT(_S, KC_F),    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},*/
-                               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SPACE_FN2_MAC, _______, _______, KC_F18, KC_RGUI, _______),
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+   	_______, _______, _______, _______, _______, SPACE_FN2_MAC, _______, _______, KC_F18, KC_RGUI, _______),
 
     /* WOW    wow 등 게임용 숫자를 간편하게 누를수있는
      * ,-----------------------------------------------------------------------------------.
@@ -1011,8 +1012,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
            */
 
             /*
-      =======
-      >>>>>>> upstream/master
       */
             /*
           case BACKLIT:
